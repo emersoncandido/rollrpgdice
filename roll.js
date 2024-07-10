ea6003b598e3792ed39dbeb64c7cea343306bc39
Emerson Cozzi Candido <emersoncandido@mec.gov.br>
2024-07-10 10:11:02 -0300

    feat: first commit

---
+++ roll.js
@@ -0,0 +1,81 @@
+const FIXED_ROWS = 7;
+
+const validRolls = () => {
+    let diceCount = 0;
+    for(d = 0; d < FIXED_ROWS; d++) {
+        diceCount += parseInt(document.getElementById('how-many-dice-' + (d)).value);
+    }
+    return diceCount > 0 ? true : false;
+}
+
+const rollDie = (type) => {
+    return Math.floor(Math.random() * type) + 1;
+}
+
+const diceNumberByRow = (row) => {
+    return parseInt(document.getElementById('how-many-dice-' + row).value);
+}
+
+const dieTypeByRow = (row) => {
+    return parseInt(document.getElementById('die-type-' + row).value);
+}
+
+const generateRows = () => {
+    let rows = [];
+    for(l = 0; l < FIXED_ROWS; l++) {
+        rows.push({'number': 0, 'type': 20, 'rolls': [], 'sum': 0});
+    }
+    return rows;
+}
+
+const rollEachRow = () => {
+    let completeRoll = generateRows();
+    for(l = 0; l < FIXED_ROWS; l++) {
+        completeRoll[l].number = diceNumberByRow(l);
+        completeRoll[l].type = dieTypeByRow(l);
+        if(diceNumberByRow(l) > 0) {
+            for(n = 0; n < diceNumberByRow(l); n++) {
+                let singleRoll = rollDie(dieTypeByRow(l));
+                completeRoll[l].rolls.push(singleRoll);
+                completeRoll[l].sum += singleRoll;
+            }
+        }
+    }
+    return completeRoll;
+}
+
+const assembleResults = () => {
+    const finishedRoll = rollEachRow();
+    // console.log(finishedRoll); return false;
+    let rowHtml = ``;
+    for(l = 0; l < FIXED_ROWS; l++) {
+        if(finishedRoll[l].number > 0) {
+            rowHtml += `<tr>`;
+            rowHtml += `<td>${finishedRoll[l].number}d${finishedRoll[l].type}</td>`;
+            rowHtml += `<td>`;
+            for(n = 0; n < finishedRoll[l].number; n++) {
+                rowHtml += `<span>${finishedRoll[l].rolls[n]}</span>`;
+            }
+            rowHtml += `</td>`;
+            rowHtml += `<td>${finishedRoll[l].sum}</td>`;
+            rowHtml += `</tr>`;
+        }
+    }
+    document.querySelector("#result-box section table tbody").innerHTML = rowHtml;
+}
+
+const showResultBox = () => {
+    assembleResults();
+    document.getElementById('result-box').style.display = 'block';
+}
+
+const rollMyDice = () => {
+    if(validRolls()) {
+        showResultBox();
+    } else {
+        alert('Select at least one dice combination before rolling.');
+    }
+}
+
+const rollButton = document.getElementById('roll-btn');
+rollButton.addEventListener('click', rollMyDice);
