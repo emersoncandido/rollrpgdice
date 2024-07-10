6475c32db2eac9f682021f650440da82205cca8d
Emerson Cozzi Candido <emersoncandido@mec.gov.br>
2024-07-10 16:11:13 -0300

    feat: makes rows of dice dinamically based on FIXED_ROWS const

--- roll.js
+++ roll.js
@@ -1,9 +1,29 @@
 const FIXED_ROWS = 7;
+const PRE_SELECTED_TYPES = ['n0t4', 'n1t6', 'n2t8', 'n3t10', 'n4t12', 'n5t20', 'n6t100'];
+const OPTION_NUMBER = 20;
+const OPTION_TYPES = [4, 6, 8, 10, 12, 20, 100];
+
+const generateOptions = () => {
+    let optionsHtml = ``;
+    for(l = 0; l < FIXED_ROWS; l++) {
+        optionsHtml += `<div class="dice-options-how-many">Roll <select id="how-many-dice-${l}">`;
+        for(n = 0; n <= OPTION_NUMBER; n++) {
+            optionsHtml += `<option value="${n}"${n == 0 ? ' selected' : ''}>${n}</option>`;
+        }
+        optionsHtml += `</select></div><div><select id="die-type-${l}">`;
+        OPTION_TYPES.forEach(
+            (type) => 
+                optionsHtml += `<option value="${type}"${('n' + l + 't' + type) == PRE_SELECTED_TYPES[l] ? ' selected' : ''}>${type}</option>`
+        );
+        optionsHtml += `</select>-sided dice</div>`;
+    }
+    document.querySelector(".dice-options").innerHTML = optionsHtml;
+}
 
 const validRolls = () => {
     let diceCount = 0;
     for(d = 0; d < FIXED_ROWS; d++) {
-        diceCount += parseInt(document.getElementById('how-many-dice-' + (d)).value);
+        diceCount += parseInt(document.getElementById('how-many-dice-' + d).value);
     }
     return diceCount > 0 ? true : false;
 }
@@ -41,12 +61,12 @@ const rollEachRow = () => {
             }
         }
     }
+    console.log(completeRoll);
     return completeRoll;
 }
 
 const assembleResults = () => {
     const finishedRoll = rollEachRow();
-    // console.log(finishedRoll); return false;
     let rowHtml = ``;
     for(l = 0; l < FIXED_ROWS; l++) {
         if(finishedRoll[l].number > 0) {
@@ -79,3 +99,5 @@ const rollMyDice = () => {
 
 const rollButton = document.getElementById('roll-btn');
 rollButton.addEventListener('click', rollMyDice);
+
+generateOptions();
